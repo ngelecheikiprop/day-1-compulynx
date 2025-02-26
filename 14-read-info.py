2ebf295ef5faf251629a7562b470898c7dd319cd
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-02-26 14:19:49 +0300

    get invoice number and date for a speicif template

--- 14-read-info.py
+++ 14-read-info.py
@@ -7,4 +7,8 @@ for page in reader.pages:
     #print(page.extract_text())
     txt = page.extract_text()
     customer_pin = re.findall(r"PIN\sNo.\s(\w\d+\w)", txt)
+    invoice_number = re.findall(r"(\s\d{3}\s)", txt)
+    date = re.findall(r"\d{2}th\s\w+\s\d{4}", txt)
     print("Customer Pin:{}".format(customer_pin[0]))
+    print("Invoice number:{}".format(invoice_number[0]))
+    print("Date:{}".format(date[0]))
